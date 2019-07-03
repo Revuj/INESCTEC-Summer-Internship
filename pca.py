f1b68b95a62e2c3b6166117463f3fc59291ab815
Revuj <revuj112@gmail.com>
2019-07-03 22:28:19 +0100

    Removed unnecessary stuff

--- pca.py
+++ pca.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from sklearn.decomposition import PCA
 
 
@@ -8,27 +7,14 @@ def main():
     datamatrix = dataframe.values[:, 1:]
     pca = PCA(n_components=10)
     pca.fit(datamatrix)
-    #PCA(copy=True, iterated_power='auto', n_components=10, random_state=None,
-     #   svd_solver='auto', tol=0.0, whiten=False)
+
+    components = pd.DataFrame(data=pca.components_)
+    components.to_csv('pca_components.csv')
 
 
-    dataframe = pd.DataFrame(data=pca.components_)
-    dataframe.to_csv('output3.csv')
-    np.savetxt("pca_components.csv", np.asarray(pca.components_), delimiter=",")
-    np.savetxt("pca_components_abs.csv", abs(np.asarray(pca.components_)), delimiter=",")
-    np.savetxt("pca_variance.csv", np.asarray(pca.explained_variance_ratio_), delimiter=",")
+    variance_ration = pd.DataFrame(data=pca.explained_variance_ratio_)
+    variance_ration.to_csv('explained_variance_racio.csv')
 
-
-    metrics_weights = []
-    for line in abs(pca.components_):
-        total = sum(line)
-        line_list = []
-        for attribute in line:
-            weight = attribute / total
-            line_list.append(weight)
-        metrics_weights.append(line_list)
-
-    np.savetxt("metric_weights.csv", np.asarray(metrics_weights), delimiter=",")
 
 
 if __name__ == '__main__':
