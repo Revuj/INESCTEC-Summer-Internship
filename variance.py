3f56376e4e1f7fb67e6f2d82599ba6bf673ead91
Revuj <revuj112@gmail.com>
2019-07-09 16:15:31 +0100

    Changed variance.py

--- variance.py
+++ variance.py
@@ -8,17 +8,24 @@ def main():
 
     content = [x.strip() for x in content]
 
-    dataframe = pd.read_csv("out2.csv")
+    same = pd.read_csv("normalized_ratios/normalized178.csv")
+    diff = pd.read_csv("normalized_ratios_same/178.csv")
 
-    mean_list = []
-    var_list = []
+    mean_same = []
+    var_same = []
+    mean_diff = []
+    var_diff = []
 
     for metric in content:
-        mean_list.append(dataframe[metric].mean())
-        var_list.append(dataframe[metric].var())
+        mean_same.append(same[metric].mean())
+        var_same.append(same[metric].var())
+        mean_diff.append(diff[metric].mean())
+        var_diff.append(diff[metric].var())
 
-    np.savetxt("means.csv", mean_list, delimiter=",", fmt='%s')
-    np.savetxt("variances.csv", var_list, delimiter=",", fmt='%s')
+    np.savetxt("comp/mean_sam178.csv", mean_same, delimiter=",", fmt='%s')
+    np.savetxt("comp/var_sam178.csv", var_same, delimiter=",", fmt='%s')
+    np.savetxt("comp/mean_dif178.csv", mean_diff, delimiter=",", fmt='%s')
+    np.savetxt("comp/var_dif178.csv", var_diff, delimiter=",", fmt='%s')
 
 
 if __name__ == '__main__':
